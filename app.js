3f9f911b7832a684226269bbc8d9f53a76d99a4e
CRISTIAN <cristianpaladines21@gmail.com>
2025-08-02 21:49:55 -0500

    Finalizada la funcion para enlistar los amigos

--- app.js
+++ app.js
@@ -14,9 +14,26 @@ function agregarAmigo(){
         alert("Por favor, ingresa un nombre de amigo.");
     }
     limpiarCaja();
+    mostrarAmigos();
 }
 
 function limpiarCaja(){
     document.getElementById("amigo").value = "";
+    console.log("Probando limpiar caja")
+}
+
+function mostrarAmigos(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i=0; i < amigos.length; i++){
+        console.log("Lista para for: ", amigos[i]);
+        lista.innerHTML = `<li>${amigos[i]}</li>` + lista.innerHTML;
+    }
+}
+
+function sortearAmigo(){
+    console.log("HOLA MUNDO DESDE SORTEAR")
+    let prueba = Math.floor(Math.random()*amigos.length);
+    console.log(prueba) 
 }
 
